f43d307d75411271e0157c0368036882f61b2cd5
e.karimi <abnoseek@gmail.com>
2024-08-31 12:44:57 +0330

    init

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button, Stack } from "@mui/material";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+    </>
+  );
+}
+
+export default App;
